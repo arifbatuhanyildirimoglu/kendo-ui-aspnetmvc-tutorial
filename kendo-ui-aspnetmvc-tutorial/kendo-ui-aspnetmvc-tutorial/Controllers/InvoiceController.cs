4e787430132ee8a74dca805911e2df5095e72701
Arif Batuhan Yıldırımoğlu <64323875+arifbatuhanyildirimoglu@users.noreply.github.com>
2024-08-19 15:55:17 +0300

    Add invoice grid to the home page
    Modify grid

--- kendo-ui-aspnetmvc-tutorial/kendo-ui-aspnetmvc-tutorial/Controllers/InvoiceController.cs
+++ kendo-ui-aspnetmvc-tutorial/kendo-ui-aspnetmvc-tutorial/Controllers/InvoiceController.cs
@@ -5,8 +5,6 @@ using System.Web.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Telerik.Scaffolders.Models.Grid;
 using KendoQsBoilerplate;
 
 namespace Telerik.Scaffolders.Controllers
